930f128b9e87e3cecb268f669effaffa4f22dee5
subratiswaroop <162547197+subratiswaroop@users.noreply.github.com>
2024-07-18 12:36:32 +0530

    Update index.js

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -2,13 +2,4 @@
 function redirect(){
     window.location.href = "mailto:subratiswaroop15@gmail.com";
 }
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
 
-burger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-  burger.classList.toggle('toggle');
-});
-
-
-const scrollLinks = document.querySelectorAll('.scrollLink');
